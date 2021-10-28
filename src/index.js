879cca3a8b5d2779f019ad1122b69dd7f45c69f0
João Victor Pistorio <joao.pistorio@gmail.com>
2021-10-28 20:42:32 -0300

    :building_construction: Creating folders for organization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import '../src/assets/css/index.css';
 import App from './App.js';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
