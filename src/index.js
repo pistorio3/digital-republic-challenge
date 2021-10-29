d484ceb809897c7a8ab7740a35e98e1f9a5fef92
João Victor Pistorio <joao.pistorio@gmail.com>
2021-10-28 22:29:48 -0300

    :rotating_light: Fix lint warnings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../src/assets/css/index.css';
-import App from './App.js';
+import './assets/css/index.css';
+import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
