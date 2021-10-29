b35ad021a9a1b107fd0611080ae142217c1902cc
João Victor Pistorio <joao.pistorio@gmail.com>
2021-10-28 21:16:00 -0300

    :building_construction: Componentization of inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import '../src/assets/css/App.css';
+import Wall from '../src/components/Wall'
 
 function App() {
   return (
@@ -8,75 +9,19 @@ function App() {
      
      <div className="container-inputs">
         <div className="input-measure">
-          <h5>Parede 1</h5>
-          <label htmlFor="wallone">
-            Parede 1
-            <input id="wallone" className="input-wall" type="number" placeholder="0.00"/>
-          </label>
-         
-          <label htmlFor="doorone" >
-            Portas
-            <input id="doorsone" className="input-door-window" type="number" placeholder="0"/>
-          </label>
-
-          <label htmlFor="windowone" >
-            Janelas
-            <input id="windowsone" className="input-door-window" type="number" placeholder="0"/>
-          </label>
-       </div>
-
-       <div className="input-measure">
-          <h5>Parede 2</h5>
-          <label htmlFor="walltwo">
-            Parede 2
-            <input id="walltwo" className="input-wall" type="number" placeholder="0.00"/>
-          </label>
-         
-          <label htmlFor="doortwo" >
-            Portas
-            <input id="doorstwo" className="input-door-window" type="number" placeholder="0"/>
-          </label>
-
-          <label htmlFor="windowtwo" >
-            Janelas
-            <input id="windowstwo" className="input-door-window" type="number" placeholder="0"/>
-          </label>
+          <Wall numberWall={1}></Wall>
         </div>
 
         <div className="input-measure">
-          <h5>Parede 3</h5>
-          <label htmlFor="wallthree">
-            Parede 3
-            <input id="wallthree" className="input-wall" type="number" placeholder="0.00"/>
-          </label>
-         
-          <label htmlFor="doorthree" >
-            Portas
-            <input id="doorsthree" className="input-door-window" type="number" placeholder="0"/>
-          </label>
-
-          <label htmlFor="windowthree" >
-            Janelas
-            <input id="windowsthree" className="input-door-window" type="number" placeholder="0"/>
-          </label>
+          <Wall numberWall={2}></Wall>
         </div>
 
         <div className="input-measure">
-          <h5>Parede 4</h5>
-          <label htmlFor="wallfour">
-            Parede 4
-            <input id="wallfour" className="input-wall" type="number" placeholder="0.00"/>
-          </label>
-         
-          <label htmlFor="doorfour" >
-            Portas
-            <input id="doorsfour" className="input-door-window" type="number" placeholder="0"/>
-          </label>
+          <Wall numberWall={3}></Wall>
+        </div>
 
-          <label htmlFor="windowfour" >
-            Janelas
-            <input id="windowsfour" className="input-door-window" type="number" placeholder="0"/>
-          </label>
+        <div className="input-measure">
+          <Wall numberWall={4}></Wall>
         </div>
 
         <button>Calcular</button>
---
+++ src/components/Wall.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class Wall extends Component {
+  render() {
+    const { numberWall,  } = this.props;
+
+
+    return (
+      <div>
+        <h5>Parede {numberWall}</h5>
+          <label htmlFor="wall-width">
+            Largura
+            <input id="wall-width" className="input-wall" type="number" placeholder="0.00"/>
+          </label>
+
+          <label htmlFor="wall-height">
+            Altura
+            <input id="wall-height" className="input-wall" type="number" placeholder="0.00"/>
+          </label>
+         
+          <label htmlFor="doors" >
+            Portas
+            <input id="doors" className="input-door-window" type="number" placeholder="0" value={0}/>
+          </label>
+
+          <label htmlFor="windows" >
+            Janelas
+            <input id="windows" className="input-door-window" type="number" placeholder="0" value={0}/>
+          </label>
+      </div>
+    )
+  }
+}
